815c72b78e4d538c6a8aa163a05c6d1d233e0564
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-04-27 11:46:21 +0900

    Convert README.md to README.rst

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,18 @@
 # Changelog
 
-## [0.0.1] - April 25, 2018
+## [0.0.2] - April 27, 2018
+### Added
+
+### Changed
+ - Convert README.md to README.rst
+
+### Removed
+N/A
+
+
+# Changelog
+
+## [0.0.1] - April 26, 2018
 ### Added
  - Initialize version.
 
--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 import codecs
 import os.path
 import re
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -21,25 +20,17 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-    long_description = long_description.replace("\r", "")  # YOU  NEED THIS LINE
-except(IOError, ImportError):
-    print("Pandoc not found. Long_description conversion failure.")
-    long_description = open('README.md').read()
-
-requires = ['requests==2.18.4', "eth-keyfile==0.5.0", "secp256k1==0.13.2", "certifi==2018.4.16", "pypandoc==1.4"]
+requires = ['requests==2.18.4', "eth-keyfile==0.5.0", "secp256k1==0.13.2", "certifi==2018.4.16"]
 
 setup_options = {
     'name': 'iconsdk', 'version': find_version("icx", "__init__.py"),
     'description': 'ICON SDK for python',
-    'long_description': long_description,
+    'long_description': open('README.rst').read(),
     'author': 'ICON foundation',
     'author_email': 'foo@icon.foundation',
     'url': 'https://github.com/icon-project/icon_sdk_for_python',
     'packages': find_packages(exclude=['tests*']),
-    'package_data': {'icon_sdk_for_python': 'README.rst'},
+    'package_data': {'iconsdk': 'README.rst'},
     'license': "Apache License 2.0",
     'install_requires': requires,
     'classifiers': [

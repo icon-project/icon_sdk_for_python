7be61613d12860c1c373db5517c5d6e365d7f9ce
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-07-10 09:58:11 +0900

    Add docstring of wallet api methods

--- icon/wallet/wallet.py
+++ icon/wallet/wallet.py
@@ -10,7 +10,7 @@ class Wallet:
     @staticmethod
     def __generate_the_version_wallet_api(version: str, api_uri: str, wallet_info: dict, address: str,
                                           hex_public_key: str) -> object:
-        """ Generate a target version wallet api object and return it.
+        """Generate a target version wallet api object and return it.
 
         :param version: a version of wallet api
         :param wallet_info: dictionary data of keystore file
@@ -27,9 +27,10 @@ class Wallet:
 
     @classmethod
     def create_wallet_by_private_key(cls, version: str, api_uri: str, hex_private_key: str, password: str):
-        """ Create a wallet object by private key without keystore file.
+        """Create a wallet object by private key without keystore file.
 
         :param version: a version of wallet api
+        :param api_uri: uri of target api
         :param hex_private_key: a private key in hexadecimals
         :return: an object of the version wallet api
         """
@@ -40,9 +41,10 @@ class Wallet:
 
     @classmethod
     def create_keystore_file_of_wallet(cls, version: str, api_uri: str, keystore_file_path: str, keystore_file_password: str):
-        """ Create a wallet object and keystore file on file path.
+        """Create a wallet object and keystore file on file path.
 
         :param version: a version of wallet api
+        :param api_uri: uri of target api
         :param keystore_file_path: file path of keystore file
         :param keystore_file_password: password of keystore file
         :return: an object of the version wallet api
@@ -55,10 +57,10 @@ class Wallet:
 
     @classmethod
     def open_keystore_file_of_wallet(cls, version: str, api_uri: str, keystore_file_path, keystore_file_password):
-        """ Open a wallet being on keystore file path and return a wallet object.
+        """Open a wallet being on keystore file path and return a wallet object.
 
         :param version: a version of wallet api
-        :param api_uri: uri of wallet api
+        :param api_uri: uri of target api
         :param keystore_file_path: file path of keystore file
         :param keystore_file_password: password of keystore file
         :return: an object of the version wallet api
@@ -98,41 +100,94 @@ class AbstractWalletApi(metaclass=ABCMeta):
     def __print_invalid_version(self):
         print("No support the api in the version")
 
-    def get_private_key(self, password):
-        self.__print_invalid_version()
-
-    def transfer_value_by_private_key(self, hex_private_key: str, to: str, amount: str, fee: str):
-        self.__print_invalid_version()
-
-    def transfer_value(self, password: str, to: str, amount: str, fee: str):
-        self.__print_invalid_version()
-
     def get_wallet_info(self):
-        self.__print_invalid_version()
+        """Get information of wallet.
 
-    def get_balance(self):
+        :return: Wallet information. type(dict).
+        """
         self.__print_invalid_version()
 
     def get_address(self):
         self.__print_invalid_version()
 
-    def get_block_by_height(self, height: str):
+    def get_private_key(self, password):
         self.__print_invalid_version()
 
-    def get_block_by_hash(self, hash: str):
+    def transfer_value_by_private_key(self, hex_private_key: str, address_to: str, amount: str, fee: str):
+        self.__print_invalid_version()
+
+    def transfer_value(self, password: str, address_to: str, amount: str, fee: str):
         self.__print_invalid_version()
 
     def get_last_block(self):
+        """Get information of the last block.
+
+        :return: Response
+        """
+        self.__print_invalid_version()
+
+    def get_block_by_height(self, height: str):
+        """Get information of the block of that height.
+
+        :param height: height of that block. type(int).
+        :return: Response
+        """
+        self.__print_invalid_version()
+
+    def get_block_by_hash(self, hash: str):
+        """ Get information of the block of that hash.
+
+        :param hash: hash value of the block which is used for ​​an digital signature. A string of 64 hexadecimal digits.
+        :return: Response
+        """
+        self.__print_invalid_version()
+
+    def icx_call(self, address_from, address_to, data_method, data_params):
+        """Call an external method in SCORE.
+
+
+        :param address_from: an address which send transaction
+        :param address_to: an SCORE's address
+        :param data_method: an external method of SCORE
+        :param data_params: parameters of the external method
+        :return: response
+        """
+        self.__print_invalid_version()
+
+    def get_balance(self):
+        """Get an balance from the address of SCORE or wallet.
+
+        :return: Response
+        """
+        self.__print_invalid_version()
+
+    def get_score_api(self, address_score):
+        """Get information of SCORE's api methods.
+
+        :param address_score: an address of SCORE
+        :return: Response
+        """
         self.__print_invalid_version()
 
     def get_total_supply(self):
+        """Get total amount of coins currently issued
+        :return: Response
+        """
         self.___print_invalid_verion()
 
-    def get_score_api(self):
+    def get_tx_result(self, hash):
+        """Get result of transaction by that hash.
+
+        :param hash: a hash value of transaction
+        :return: Response
+        """
         self.__print_invalid_version()
 
-    # check point: icx_call 제공 유무
-    def icx_call(self):
+    def get_tx_by_hash(self):
+        """Get information of transaction by hash.
+
+        :return: Response
+        """
         self.__print_invalid_version()
 
 
@@ -172,40 +227,40 @@ class WalletApiV3(AbstractWalletApi):
     """ V3에서 제공하는 Wallet API 메소드를 정의한 자식 클래스 """
 
     def get_private_key(self, password):
-        self.__print_invalid_version()
+        pass
 
     def transfer_value_by_private_key(self, hex_private_key: str, to: str, amount: str, fee: str):
-        self.__print_invalid_version()
+        pass
 
     def transfer_value(self, password: str, to: str, amount: str, fee: str):
-        self.__print_invalid_version()
+        pass
 
     def get_wallet_info(self):
-        self.__print_invalid_version()
+        pass
 
     def get_balance(self):
-        self.__print_invalid_version()
+        pass
 
     def get_address(self):
-        self.__print_invalid_version()
+        pass
 
     def get_block_by_height(self, height: str):
-        self.__print_invalid_version()
+        pass
 
     def get_block_by_hash(self, hash: str):
-        self.__print_invalid_version()
+        pass
 
     def get_last_block(self):
-        self.__print_invalid_version()
+        pass
 
     def get_total_supply(self):
-        self.___print_invalid_verion()
+        pass
 
     def get_score_api(self):
-        self.__print_invalid_version()
+        pass
 
     def icx_call(self):
-        self.__print_invalid_version()
+        pass
 
 # v2
 from icx.wallet import Wallet
